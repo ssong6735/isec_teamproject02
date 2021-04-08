a70a18df0f585ba4ee1e14a2f5724dc82b58fd80
ssong6735 <because6735@gmail.com>
2021-04-08 19:00:53 +0900

    [21.04.08] 일단은 현재까지 병합한거

--- src/main/java/com/prison_management/prison_mg/cli/prison/repository/MemoryPrisonRepository.java
+++ src/main/java/com/prison_management/prison_mg/cli/prison/repository/MemoryPrisonRepository.java
@@ -16,16 +16,16 @@ public class MemoryPrisonRepository implements PrisonRepository {
 
     private static void insertTestData() {
 //        Prison prison1 = new prisoner("이름","나이","지역","죄목","형량");
-        Prison prison1 = new Prison("김제혁",35,"천안","폭행치사",5);
-        Prison prison2 = new Prison("명교수",43,"서울","사기",2);
-        Prison prison3 = new Prison("김민철",52,"광주","살인",25);
-        Prison prison4 = new Prison("강철두",44,"인천","사기도박",3);
-        Prison prison5 = new Prison("이주형",23,"대전","상습절도",1);
-        Prison prison6 = new Prison("유한양",26,"서울","마약복용",1);
-        Prison prison7 = new Prison("유정우",26,"대전","상해치사",3);
-        Prison prison8 = new Prison("법자",25,"청주","상습절도",2);
-        Prison prison9 = new Prison("고박사",31,"부산","배임횡령",5);
-        Prison prison10 = new Prison("똘마니",24,"인천","상습폭행",4);
+        Prison prison1 = new Prison("김제혁",35,"천안","폭행치사",5000);
+        Prison prison2 = new Prison("명교수",43,"서울","사기",2000);
+        Prison prison3 = new Prison("김민철",52,"광주","살인",25000);
+        Prison prison4 = new Prison("강철두",44,"인천","사기도박",3000);
+        Prison prison5 = new Prison("이주형",23,"대전","상습절도",1000);
+        Prison prison6 = new Prison("유한양",26,"서울","마약복용",1000);
+        Prison prison7 = new Prison("유정우",26,"대전","상해치사",3000);
+        Prison prison8 = new Prison("법자",25,"청주","상습절도",2000);
+        Prison prison9 = new Prison("고박사",31,"부산","배임횡령",5000);
+        Prison prison10 = new Prison("똘마니",24,"인천","상습폭행",4000);
 
         prisonMemoryDB.put(prison1.getPrisonerNumber(), prison1);
         prisonMemoryDB.put(prison2.getPrisonerNumber(), prison2);
